a3010b11bbee3e8ca81b208e1832330604df9821
Francois Lecerf <flecerf2511@gmail.com>
2017-12-30 20:35:46 +0100

    improved import speed

--- jvdp_workflow/data.py
+++ jvdp_workflow/data.py
@@ -4,8 +4,8 @@ from urllib.request import urlretrieve
 
 URL_FREMONT = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
 
-def get_fremont_data(filename='Fremont.csv', 
-                     url = URL_FREMONT, 
+def get_fremont_data(filename='Fremont.csv',
+                     url = URL_FREMONT,
                      force_download=False):
     """Download and cache the Fremont data
 
@@ -24,7 +24,11 @@ def get_fremont_data(filename='Fremont.csv',
     """
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
-    data = pd.read_csv(filename, index_col='Date', parse_dates=True)
+    data = pd.read_csv(filename, index_col='Date')
+    try:
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+    except TypeError:
+        data.index = pd.to_datetime(data.index)
     data.columns = ['West', 'East']
     data['Total'] = data.West + data.East
     return data
