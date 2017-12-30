26b5fd135b0642abc2860c362c28e986cba804a8
Francois Lecerf <flecerf2511@gmail.com>
2017-12-30 19:19:50 +0100

    new release

---
+++ jvdp_workflow/data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+from urllib.request import urlretrieve
+
+URL_FREMONT = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+def get_fremont_data(filename='Fremont.csv', 
+                     url = URL_FREMONT, 
+                     force_download=False):
+    """Download and cache the Fremont data
+
+    Parameters
+    ----------
+    filename : string (optional)
+               location to save the data
+    url : string (optional)
+          web location of the data
+    force_download : bool (optional)
+                     If True then force data download
+    Returns
+    -------
+    data: pandas Dataframe
+          the Fremont bridge data
+    """
+    if force_download or not os.path.exists(filename):
+        urlretrieve(url, filename)
+    data = pd.read_csv(filename, index_col='Date', parse_dates=True)
+    data.columns = ['West', 'East']
+    data['Total'] = data.West + data.East
+    return data
